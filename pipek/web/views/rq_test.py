508475033db00ee48c1b71f54046637bab37bc8e
Thanathip Limna <boatkrap@gmail.com>
2024-09-09 13:15:19 +0700

    fix rq for multiple load

--- pipek/web/views/rq_test.py
+++ pipek/web/views/rq_test.py
@@ -6,13 +6,17 @@ from pipek.jobs import hello_rq
 
 module = Blueprint("rq-test", __name__, url_prefix="/rq-test")
 
+job_id = 0
+
 
 @module.route("/")
 def index():
+    global job_id
+    job_id += 1
     job = redis_rq.redis_queue.queue.enqueue(
         hello_rq.say_hello_rq,
-        args=("thanathip",),
-        job_id=f"hello-01",
+        args=(f"thanathip - {job_id}",),
+        job_id=f"hello-{job_id}",
         timeout=600,
         job_timeout=600,
     )
@@ -23,5 +27,5 @@ def index():
 @module.route("/state")
 def check_job_state():
 
-    job = redis_rq.redis_queue.get_job("hello-01")
+    job = redis_rq.redis_queue.get_job(f"hello-{job_id}")
     return f"Hello world {job.id} {job.result}"
